019a8374dd2d7157eb6b17e48dbd0926185e1234
Steve Winton <stevewinton@gmail.com>
2013-03-26 15:36:07 -0400

    Added generate_oauth_header to close.consumer.utils

--- src/close/consumer/utils.py
+++ src/close/consumer/utils.py
@@ -10,6 +10,8 @@ import random
 import time
 import urllib
 
+from . import oauth
+
 
 def unicode_urlencode(params):
     if isinstance(params, dict):
@@ -36,3 +38,16 @@ def generate_hash(algorithm='sha1', s=None):
 def generate_auth_header(username, password):
     auth = base64.b64encode(u'%s:%s' % (username, password))
     return 'Basic %s' % auth
+
+
+def generate_oauth_header(consumer_key, consumer_secret, access_key, access_secret, url, method="POST", parameters=None):
+    consumer = oauth.OAuthConsumer(consumer_key, consumer_secret)
+    token = oauth.OAuthToken(access_key, access_secret)
+    sigmethod = oauth.OAuthSignatureMethod_HMAC_SHA1()
+    request = oauth.OAuthRequest.from_consumer_and_token(
+        consumer, http_url=url, http_method=method,
+        token=token, parameters=parameters
+    )
+    request.sign_request(sigmethod, consumer, token)
+    header = request.to_header()
+    return header["Authorization"]
